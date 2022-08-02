349a578a4519ecf8c1bb797fd102e1780d17e4a7
Shirleen <gathungushirleen@gmail.com>
2022-08-02 23:37:53 +0300

    added country choices

--- wallet/models.py
+++ wallet/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import email
 from email import message
+
 from email.headerregistry import Address
 from locale import currency
 from pydoc import ModuleScanner
@@ -10,30 +11,272 @@ from xmlrpc.client import DateTime
 from django.db import models
 
 
-# Create your models here.
+# Create your models here.  
 
 
 class Customer(models.Model):
     first_name=models.CharField(max_length=20)
     last_name=models.CharField(max_length=20)
-    address=models.TextField()
+    address=models.CharField(max_length=95)
     email=models.EmailField(max_length=254)
     phone_number=models.CharField(max_length=13)
-    gender=models.CharField(max_length=10)
+    GENDER_CHOICES = (
+           ("M", "Male"),
+           ("F", "Female"),
+           ("NB", "Non-Binary"),
+    )
+    gender=models.CharField(max_length=10,choices=GENDER_CHOICES)
     age=models.PositiveSmallIntegerField()
     profile_picture = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
 
 class Currency(models.Model):
-    country= models.CharField(max_length=30)
-    symbol= models.CharField(max_length=5)
+    COUNTRY_CHOICES = (
+('AFG','Afghanistan'),
+('AF','Afghanistan'),
+('ZA','Afrique du Sud'),
+('AL','Albanie'),
+('DZ','Algérie'),
+('DE','Allemagne'),
+('AD','Andorre'),
+('AO','Angola'),
+('AI','Anguilla'),
+('AQ','Antarctique'),
+('AG','Antigua-et-Barbuda'),
+('AN','Antilles néerlandaises'),
+('SA','Arabie saoudite'),
+('AR','Argentine'),
+('AM','Arménie'),
+('AW','Aruba'),
+('AU','Australie'),
+('AT','Autriche'),
+('AZ','Azerbaïdjan'),
+('BJ','Bénin'),
+('BS','Bahamas'),
+('BH','Bahreïn'),
+('BD','Bangladesh'),
+('BB','Barbade'),
+('PW','Belau'),
+('BE','Belgique'),
+('BZ','Belize'),
+('BM','Bermudes'),
+('BT','Bhoutan'),
+('BY','Biélorussie'),
+('MM','Birmanie'),
+('BO','Bolivie'),
+('BA','Bosnie-Herzégovine'),
+('BW','Botswana'),
+('BR','Brésil'),
+('BN','Brunei'),
+('BG','Bulgarie'),
+('BF','Burkina Faso'),
+('CA','Canada'),
+('CV','Cap-Vert'),
+('CL','Chili'),
+('CN','Chine'),
+('CY','Chypre'),
+('CO','Colombie'),
+('KM','Comores'),
+('CG','Congo'),
+('KP','Corée du Nord'),
+('KR','Corée du Sud'),
+('CR','Costa Rica'),
+('HR','Croatie'),
+('CU','Cuba'),
+('DK','Danemark'),
+('DJ','Djibouti'),
+('DM','Dominique'),
+('EG','Égypte'),
+('AE','Émirats arabes unis'),
+('EC','Équateur'),
+('ER','Érythrée'),
+('ES','Espagne'),
+('EE','Estonie'),
+('US','États-Unis'),
+('ET','Éthiopie'),
+('FI','Finlande'),
+('FR','France'),
+('GE','Géorgie'),
+('GA','Gabon'),
+('GM','Gambie'),
+('GH','Ghana'),
+('GI','Gibraltar'),
+('GR','Grèce'),
+('GD','Grenade'),
+('GL','Groenland'),
+('GP','Guadeloupe'),
+('GU','Guam'),
+('GT','Guatemala'),
+('GN','Guinée'),
+('GQ','Guinée équatoriale'),
+('GW','Guinée-Bissao'),
+('GY','Guyana'),
+('GF','Guyane française'),
+('HT','Haïti'),
+('HN','Honduras'),
+('HK','Hong Kong'),
+('HU','Hongrie'),
+('BV','Ile Bouvet'),
+('CX','Ile Christmas'),
+('NF','Ile Norfolk'),
+('KY','Iles Cayman'),
+('CK','Iles Cook'),
+('FO','Iles Féroé'),
+('FK','Iles Falkland'),
+('FJ','Iles Fidji'),
+('GS','Iles Géorgie du Sud et Sandwich du Sud'),
+('HM','Iles Heard et McDonald'),
+('MH','Iles Marshall'),
+('PN','Iles Pitcairn'),
+('SB','Iles Salomon'),
+('SJ','Iles Svalbard et Jan Mayen'),
+('TC','Iles Turks-et-Caicos'),
+('VI','Iles Vierges américaines'),
+('VG','Iles Vierges britanniques'),
+('CC','Iles des Cocos (Keeling),'),
+('UM','Iles mineures éloignées des États-Unis'),
+('IN','Inde'),
+('ID','Indonésie'),
+('IR','Iran'),
+('IQ','Iraq'),
+('IE','Irlande'),
+('IS','Islande'),
+('IL','Israël'),
+('IT','Italie'),
+('JM','Jamaïque'),
+('JP','Japon'),
+('JO','Jordanie'),
+('KZ','Kazakhstan'),
+('KE','Kenya'),
+('KG','Kirghizistan'),
+('KI','Kiribati'),
+('KW','Koweït'),
+('LA','Laos'),
+('LS','Lesotho'),
+('LV','Lettonie'),
+('LB','Liban'),
+('LR','Liberia'),
+('LY','Libye'),
+('LI','Liechtenstein'),
+('LT','Lituanie'),
+('LU','Luxembourg'),
+('MO','Macao'),
+('MG','Madagascar'),
+('MY','Malaisie'),
+('MW','Malawi'),
+('MV','Maldives'),
+('ML','Mali'),
+('MT','Malte'),
+('MP','Mariannes du Nord'),
+('MA','Maroc'),
+('MQ','Martinique'),
+('MU','Maurice'),
+('MR','Mauritanie'),
+('YT','Mayotte'),
+('MX','Mexique'),
+('FM','Micronésie'),
+('MD','Moldavie'),
+('MC','Monaco'),
+('MN','Mongolie'),
+('MS','Montserrat'),
+('MZ','Mozambique'),
+('NP','Népal'),
+('NA','Namibie'),
+('NR','Nauru'),
+('NI','Nicaragua'),
+('NE','Niger'),
+('NG','Nigeria'),
+('NU','Nioué'),
+('NO','Norvège'),
+('NC','Nouvelle-Calédonie'),
+('NZ','Nouvelle-Zélande'),
+('OM','Oman'),
+('UG','Ouganda'),
+('UZ','Ouzbékistan'),
+('PE','Pérou'),
+('PK','Pakistan'),
+('PA','Panama'),
+('PG','Papouasie-Nouvelle-Guinée'),
+('PY','Paraguay'),
+('NL','Pays-Bas'),
+('PH','Philippines'),
+('PL','Pologne'),
+('PF','Polynésie française'),
+('PR','Porto Rico'),
+('PT','Portugal'),
+('QA','Qatar'),
+('CF','République centrafricaine'),
+('CD','République démocratique du Congo'),
+('DO','République dominicaine'),
+('CZ','République tchèque'),
+('RE','Réunion'),
+('RO','Roumanie'),
+('GB','Royaume-Uni'),
+('RU','Russie'),
+('RW','Rwanda'),
+('SN','Sénégal'),
+('EH','Sahara occidental'),
+('KN','Saint-Christophe-et-Niévès'),
+('SM','Saint-Marin'),
+('PM','Saint-Pierre-et-Miquelon'),
+('VA','Saint-Siège' ),
+('VC','Saint-Vincent-et-les-Grenadines'),
+('SH','Sainte-Hélène'),
+('LC','Sainte-Lucie'),
+('SV','Salvador'),
+('WS','Samoa'),
+('AS','Samoa américaines'),
+('ST','Sao Tomé-et-Principe'),
+('SC','Seychelles'),
+('SL','Sierra Leone'),
+('SG','Singapour'),
+('SI','Slovénie'),
+('SK','Slovaquie'),
+('SO','Somalie'),
+('SD','Soudan'),
+('LK','Sri Lanka'),
+('SE','Suède'),
+('CH','Suisse'),
+('SR','Suriname'),
+('SZ','Swaziland'),
+('SY','Syrie'),
+('TW','Taïwan'),
+('TJ','Tadjikistan'),
+('TZ','Tanzanie'),
+('TD','Tchad'),
+('TF','Terres australes françaises'),
+('IO','Territoire britannique Océan Indien'),
+('TH','Thaïlande'),
+('TL','Timor Oriental'),
+('TG','Togo'),
+('TK','Tokélaou'),
+('TO','Tonga'),
+('TT','Trinité-et-Tobago'),
+('TN','Tunisie'),
+('TM','Turkménistan'),
+('TR','Turquie'),
+('TV','Tuvalu'),
+('UA','Ukraine'),
+('UY','Uruguay'),
+('VU','Vanuatu'),
+('VE','Venezuela'),
+('VN','Viêt Nam'),
+('WF','Wallis-et-Futuna'),
+('YE','Yémen'),
+('YU','Yougoslavie'),
+('ZM','Zambie'),
+('ZW','Zimbabwe'),
+('MK','ex-République yougoslave de Macédoine'),
+)
+    country= models.CharField(max_length=30,choices= COUNTRY_CHOICES)
+    
+    symbol_name= models.CharField(max_length= 20,null=True)
     amount= models.BigIntegerField()
 
 
 class Wallet(models.Model):
     balance = models.IntegerField()
     customer = models.ForeignKey(Customer,on_delete=models.CASCADE,related_name='wallet_customer')
-    amount  = models.IntegerField()
     date_created = models.DateTimeField()
     status = models.CharField(max_length=15)
     currency = models.ForeignKey(Currency,on_delete=models.CASCADE,related_name='wallet_currency')
@@ -44,7 +287,11 @@ class Wallet(models.Model):
 class Account(models.Model):
     account_name = models.CharField(max_length=20)
     account_number = models.IntegerField()
-    account_type = models.CharField(max_length=20)
+    ACCOUNT_CHOICES=(
+        ("P", "Personal Account"),
+        ("B", "Business Account"),
+    )
+    account_type = models.CharField(max_length=20,choices=ACCOUNT_CHOICES)
     account_balance = models.IntegerField()
     wallet = models.ForeignKey(Wallet,on_delete=models.CASCADE,related_name='account_wallet')
 
