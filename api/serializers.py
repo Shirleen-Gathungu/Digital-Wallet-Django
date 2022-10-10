6e69fb6c6c3ab49fb8117b8f4bcb655039b9907d
Shirleen <gathungushirleen@gmail.com>
2022-10-11 00:10:00 +0300

    created serializers,views and routes for specific models in the wallet

--- api/serializers.py
+++ api/serializers.py
@@ -2,9 +2,55 @@ from dataclasses import fields
 from pyexpat import model
 from rest_framework import serializers
 from wallet.models import Customer
+from wallet.models import Wallet
+from wallet.models import Account
+from wallet.models import Card
+from wallet.models import Transaction
+from wallet.models import Loan
+from wallet.models import Receipt
+from wallet.models import Notification
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model= Customer
         fields=("first_name","age","address","gender")
-        
+    
+
+class WalletSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Wallet
+        fields= ('balance','customer','date_created','status','currency','history','pin')
+
+
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Account
+        fields= ('account_name','account_number','account_type','account_balance','wallet')
+
+class CardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Card
+        fields= ('card_number','card_type','expiry_date','security_code','date_of_issue','wallet','account','issuer')
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Transaction
+        fields=('message','wallet','transaction_amount','transaction_charge','transaction_type','origin_account','destination_account')
+
+
+class LoanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Loan
+        fields=('loan_id','loan_type','loan_balance','amount','guaranter','issuer','wallet')
+
+
+class ReceiptSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Receipt
+        fields=('receipt_type','date','receipt_number','amount','transaction','receipt_file')
+
+class NotificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Notification
+        fields=('message','date','recipient','title')
+
--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path,include
 from rest_framework import routers
 from .views import CustomerViewSet
-
+from .views import WalletViewSet
+from .views import AccountViewSet
+from .views import CardViewSet
+from .views import TransactionViewSet
+from .views import LoanViewSet 
+from .views import ReceiptViewSet 
+from .views import NotificationViewSet 
 router = routers.DefaultRouter()
 router.register(r"customers",CustomerViewSet)
+router.register(r"wallet",WalletViewSet)
+router.register(r"account",AccountViewSet)
+router.register(r"card",CardViewSet)
+router.register(r"transaction",TransactionViewSet)
+router.register(r"loan",LoanViewSet)
+router.register(r"receipt",ReceiptViewSet)
+router.register(r"notification",NotificationViewSet)
 urlpatterns=[
     path("",include(router.urls)),
 ]
